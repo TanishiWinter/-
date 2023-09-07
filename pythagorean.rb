7e8560ffd869b84b1b842c946894c5b5c9cfdb6a
EC2 Default User <ec2-user@ip-172-31-26-182.ap-northeast-1.compute.internal>
2023-09-07 22:29:38 +0000

    [Challenge] ピタゴラス練習

---
+++ pythagorean.rb
@@ -0,0 +1,11 @@
+def pythagorean_theorem(a, b)
+  c_squared = a**2 + b**2
+  c = Math.sqrt(c_squared)
+  return c
+end
+
+# 例: a = 3, b = 4 の場合
+a = 3
+b = 4
+c = pythagorean_theorem(a, b)
+puts "a = #{a}, b = #{b} のとき、c = #{c}"
