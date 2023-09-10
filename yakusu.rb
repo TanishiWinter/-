ff026d01a08cf875028531691e3ccecebfe1c88a
EC2 Default User <ec2-user@ip-172-31-26-182.ap-northeast-1.compute.internal>
2023-09-10 00:47:25 +0000

    [Challenge] 三つ以上の約数

--- yakusu.rb
+++ yakusu.rb
@@ -32,3 +32,32 @@ num1 = 12
 num2 = 18
 result = lcm(num1, num2)
 puts "最小公倍数は #{result}"
+
+
+def gcd_of_numbers(numbers)
+  numbers.reduce { |gcd, num| gcd.gcd(num) }
+end
+
+# 数の配列を定義
+numbers = [24, 36, 48, 60]
+
+# 配列内の数の最大公約数を計算
+result = gcd_of_numbers(numbers)
+
+# 結果を表示
+puts "配列内の数の最大公約数: #{result}"
+
+
+# 最小公倍数を計算するメソッドを定義
+def lcm_of_numbers(numbers)
+  numbers.reduce(1) { |lcm, num| lcm.lcm(num) }
+end
+
+# 数の配列を定義
+numbers = [4, 6, 8, 12]
+
+# 配列内の数の最小公倍数を計算
+result = lcm_of_numbers(numbers)
+
+# 結果を表示
+puts "配列内の数の最小公倍数: #{result}"
